0ed21f3b33df0bee77364ebb934da4a747f8a89b
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2023-06-30 18:22:35 -0700

    c language

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 /**
  * _strncat - concatenate strings, defining the size of second string.
  * @dest: string with concatenation
--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,29 +1,28 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * rot13 - encoder rot13
- * @c: pointer to string params
- * Return: *c
+ * rot13 - change letters to ROT13.
+ * @s: analized string.
+ *
+ * Return: String with all letters in ROT13 base.
  */
-
-char *rot13(char *c)
+char *rot13(char *s)
 {
-	int i;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0;
 	int j;
-	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; c[i] != '\0'; i++)
+	while (*(s + i) != '\0')
 	{
-		for (j = 0; j < 52; j++)
+		for (j = 0; j <= 51; j++)
 		{
-			if (c[i] == data1[j])
+			if (*(s + i) == a[j])
 			{
-				c[i] = datarot[j];
+				*(s + i) = rot[j];
 				break;
 			}
 		}
+		i++;
 	}
-	return (c);
+	return (s);
 }
