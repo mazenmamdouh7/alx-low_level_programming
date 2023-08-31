5db26d6da8fee4f9dfb7d8614f94c2d84ec711aa
mazenmamdouh7 <mazen_mamdouh0@hotmail.com>
2023-08-30 18:54:20 -0700

    C Tasks

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ * Return: 1 if it is, 0 it's not
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == 0 && *s == *s + 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
